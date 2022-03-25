20dd8024fdc52075b19c00e9b3dbae867690ab4c
daeho kim <daeho.kim@softwareinlife.com>
2022-03-25 18:51:56 +0900

    first commit

---
+++ src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,22 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageDecorator implements Component{
+
+    private Component component;
+
+    public MessageDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        log.info("MessageDecorator 실행");
+
+        String operation = component.operation();
+        String decoResult = "******" + operation + "*******";
+        return decoResult;
+    }
+}
