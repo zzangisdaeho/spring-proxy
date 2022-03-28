13a70ed631c550c4065738a68e9f9f9b6decefa6
daeho kim <daeho.kim@softwareinlife.com>
2022-03-28 13:04:59 +0900

    compile error fix

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -12,8 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-//@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
