ab3bceb5d766003db70d5c31c753cac2a318b99f
daeho kim <daeho.kim@softwareinlife.com>
2022-04-06 19:05:23 +0900

    spring proxyfactory fin

---
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,93 @@
+package hello.proxy.proxyfactory;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ConcreteService;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    void interfaceProxy(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+
+        proxy.find();
+
+        proxy.save();
+
+        //ProxyFactory를 통해서 생성한 프록시만 사용 가능. 스프링이 내부에서 그렇게 처리한데..
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+
+    @Test
+    @DisplayName("구현체를 사용해도 인터페이스가 있다면 JDK 동적 프록시 사용")
+    void interfaceProxy2(){
+        ServiceImpl target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+
+        proxy.find();
+
+        proxy.save();
+
+        //ProxyFactory를 통해서 생성한 프록시만 사용 가능. 스프링이 내부에서 그렇게 처리한데..
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+
+    @Test
+    @DisplayName("구체 클래스만 있으면 CGLIB 사용 JDK 동적 프록시 사용")
+    void concreteProxy(){
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+
+        proxy.call();
+
+        //ProxyFactory를 통해서 생성한 프록시만 사용 가능. 스프링이 내부에서 그렇게 처리한데..
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하고, 클래스 기반 프록시 사용")
+    void proxyTargetClass() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); //중요
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+        proxy.save();
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+}
